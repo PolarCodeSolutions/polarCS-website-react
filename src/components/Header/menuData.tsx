4ce2fba7ddb87e3b511de875687819f493fc3393
bjorgeh <bjorgen92@gmail.com>
2024-06-18 22:16:16 +0200

    uploaded

---
+++ src/components/Header/menuData.tsx
@@ -0,0 +1,29 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Hjem",
+    path: "/",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "Om oss",
+    path: "/about",
+    newTab: false,
+  },
+  {
+    id: 33,
+    title: "Produkter",
+    path: "/blog",
+    newTab: false,
+  },
+  {
+    id: 3,
+    title: "Kontakt oss",
+    path: "/contact",
+    newTab: false,
+  },
+];
+export default menuData;
