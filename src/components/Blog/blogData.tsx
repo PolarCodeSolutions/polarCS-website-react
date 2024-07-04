7e0e3d9b74355662182380878aa7d6ab3650f2a7
bjorgeh <bjorgen92@gmail.com>
2024-07-04 20:22:19 +0200

    content update
    la til og endret innhold i sidene

--- src/components/Blog/blogData.tsx
+++ src/components/Blog/blogData.tsx
@@ -3,45 +3,44 @@ import { Blog } from "@/types/blog";
 const blogData: Blog[] = [
   {
     id: 1,
-    title: "Best UI components for modern websites",
-    paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+    title: "Vi lager nettside for bedriften din",
+    paragraph:"Vi tar arbeidet og hjelper med å fremstille en kravspesifikasjon, design og utvikling av nettsiden",
     image: "/images/blog/blog-01.jpg",
     author: {
-      name: "Samuyl Joshi",
+      name: "Morten Sandstedt",
       image: "/images/blog/author-01.png",
-      designation: "Graphic Designer",
+      designation: "Front-end Developer",
     },
-    tags: ["creative"],
-    publishDate: "2025",
+    tags: ["webdesign"],
+    publishDate: "2024",
   },
   {
     id: 2,
-    title: "9 simple ways to improve your design skills",
+    title: "Utvikling av programvare",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+      "Vi utvikler programvare for bedrifter og privatpersoner.",
     image: "/images/blog/blog-02.jpg",
     author: {
-      name: "Musharof Chy",
+      name: "Asgeir Bjørgen Huskebakk",
       image: "/images/blog/author-02.png",
-      designation: "Content Writer",
+      designation: "Software Developer",
     },
-    tags: ["computer"],
-    publishDate: "2025",
+    tags: ["software"],
+    publishDate: "2024",
   },
   {
     id: 3,
-    title: "Tips to quickly improve your coding speed.",
+    title: "Support og vedlikehold av nettsider og programvare",
     paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
+      "Vi tilbyr support og vedlikehold av nettsider og programvare.",
     image: "/images/blog/blog-03.jpg",
     author: {
-      name: "Lethium Deo",
-      image: "/images/blog/author-03.png",
+      name: "Polar Code Solutions",
+      image: "/images/logo/croppedPolarLogo.png",
       designation: "Graphic Designer",
     },
-    tags: ["design"],
-    publishDate: "2025",
+    tags: ["Support"],
+    publishDate: "2024",
   },
 ];
 export default blogData;
