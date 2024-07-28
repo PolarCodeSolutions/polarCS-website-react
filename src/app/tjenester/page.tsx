efc25f7bd7198e96267df100ca2f56cdda75d4bd
bjorgeh <bjorgen92@gmail.com>
2024-07-28 13:57:18 +0200

    bugcheck

--- src/app/tjenester/page.tsx
+++ src/app/tjenester/page.tsx
@@ -33,19 +33,7 @@ const Blog: React.FC = () => {
   return (
     <>
       <Head>
-        
-        {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-76N1PJZ11X"></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-76N1PJZ11X');
-            `,
-          }}
-        />
+
         
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
