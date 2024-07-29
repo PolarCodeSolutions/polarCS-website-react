1ecd67e1f7740bbe886543284a238991da8aadea
Bjorgeh <bjorgen92@gmail.com>
2024-07-29 13:20:48 +0200

    SEO-290724-1320

--- src/components/Contact/NewsLatterBox.tsx
+++ src/components/Contact/NewsLatterBox.tsx
@@ -12,7 +12,7 @@ const NewsLatterBox = () => {
     <div className="relative z-10 rounded-sm bg-white p-8 shadow-three dark:bg-gray-dark sm:p-11 lg:p-8 xl:p-11">
      
       <h3 className="mb-4 text-2xl font-bold leading-tight text-black dark:text-white">
-        Kontakt oss på andre måter
+        Vi kan nås på følgende måter:
       </h3>
       <p className="mb-11 border-b border-body-color border-opacity-25 pb-11 text-base leading-relaxed text-body-color dark:border-white dark:border-opacity-25">
         Vi prøver å være så tilgjenelige som mulig. 
