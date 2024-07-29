61dff209b2a402af80c403f8ae40c06f7a0e57ad
Bjorgeh <bjorgen92@gmail.com>
2024-07-29 13:12:16 +0200

    SEO-290724-1312

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -131,7 +131,7 @@ const Footer = () => {
                       href="/kontakt"
                       className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
                     >
-                      Fyll ut vårt kontaktskjema
+                      Send inn skjema
                     </Link>
                   </li>
                 </ul>
