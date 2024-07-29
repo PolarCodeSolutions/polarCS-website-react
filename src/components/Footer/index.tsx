dccf0f9326f5ac4c8d9ee234b5e4aa64caa61326
Bjorgeh <bjorgen92@gmail.com>
2024-07-29 12:55:45 +0200

    SEO-290724-1255

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -123,7 +123,7 @@ const Footer = () => {
             <div className="w-full px-4 sm:w-1/2 md:w-1/2 lg:w-2/12 xl:w-2/12">
               <div className="mb-12 lg:mb-16">
                 <h2 className="mb-10 text-xl font-bold text-black dark:text-white">
-                  Online hjelp og spørsmål
+                  Hjelp og spørsmål
                 </h2>
                 <ul>
                   <li>
@@ -131,7 +131,7 @@ const Footer = () => {
                       href="/kontakt"
                       className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
                     >
-                      Send oss en melding
+                      Fyll ut vårt kontaktskjema
                     </Link>
                   </li>
                 </ul>
