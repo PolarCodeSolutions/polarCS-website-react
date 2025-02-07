ea00aa064253215dd8df5464dc0d96ede26e67ff
Bjorgeh <122554284+Bjorgeh@users.noreply.github.com>
2025-02-07 15:01:07 +0100

    updated contact form

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -117,7 +117,9 @@ const ContactContent: React.FC = () => {
                         required
                         className="border-stroke w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
                         value={preFilledMessage}
+                        onChange={(e) => setPreFilledMessage(e.target.value)} // Legg til denne linjen
                       ></textarea>
+
                     </div>
                   </div>
                   <div className="w-full px-4">
